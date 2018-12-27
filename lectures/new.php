05342523cc96394ee96f1d98c596b6c0c51814c1
LaibaTahir <44542335+LaibaTahir@users.noreply.github.com>
2018-12-27 11:16:26 +0500

    Add files via upload
    Trees activity after mids.

---
+++ lectures/new.php
@@ -0,0 +1,9 @@
+<?php
+$a= array ("Trees provide oxygen and clean the air.", "Trees help prevent soil eroison.", "Trees in the landscape relax us; they lower our heart rate and reduce stress.");
+
+for ($i=0; $i<3; $i++)
+{
+	echo"<li>".$a{$i}."</li>";
+}
+
+?>
